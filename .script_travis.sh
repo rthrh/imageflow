8b099847d6cd5368931b8a0cdd7291217c955ffc
rthrh <woeflele@gmail.com>
2018-03-07 15:43:56 +0100

    add git submodule update command to travis script

--- .script_travis.sh
+++ .script_travis.sh
@@ -3,5 +3,5 @@
 docker run \
      $ci_env -e WITH_COVERAGE -e CC -e CXX -e DISPLAY=:10 --privileged --cap-add=ALL \
      -v `pwd`:/root/sources rthrh/imflow /bin/bash \
-     -c "source /opt/qt59/bin/qt59-env.sh && cd ~/ && rm -rf imageflow && git clone https://github.com/rthrh/imageflow.git && cd imageflow && mkdir build && cd build && cmake .. && make"
+     -c "source /opt/qt59/bin/qt59-env.sh && cd ~/ && rm -rf imageflow && git clone https://github.com/rthrh/imageflow.git && cd imageflow && git submodule update --init --recursive && mkdir build && cd build && cmake .. && make"
 
